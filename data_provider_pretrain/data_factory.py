ffa9b38af4a23341b8bcc86281c3bb5f98385a02
liyy2 <liyy2@shanghaitech.edu.cn>
2024-04-12 21:57:25 -0400

    update model

--- data_provider_pretrain/data_factory.py
+++ data_provider_pretrain/data_factory.py
@@ -1,12 +1,13 @@
 from torch.utils.data import DataLoader
 
-from data_provider_pretrain.data_loader import Dataset_ETT_hour, Dataset_ETT_minute
+from data_provider_pretrain.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Dataset_Combined
 
 data_dict = {
     'ETTh1': Dataset_ETT_hour,
     'ETTh2': Dataset_ETT_hour,
     'ETTm1': Dataset_ETT_minute,
     'ETTm2': Dataset_ETT_minute,
+    'Glucose': Dataset_Combined,
 }
 
 
@@ -14,7 +15,6 @@ def data_provider(args, data, data_path, pretrain=True, flag='train'):
     Data = data_dict[data]
     timeenc = 0 if args.embed != 'timeF' else 1
     percent = args.percent
-
     if flag == 'test':
         shuffle_flag = False
         drop_last = True
@@ -25,20 +25,25 @@ def data_provider(args, data, data_path, pretrain=True, flag='train'):
         drop_last = True
         batch_size = args.batch_size
         freq = args.freq
-
-    data_set = Data(
-        root_path=args.root_path,
-        data_path=data_path,
-        flag=flag,
-        size=[args.seq_len, args.label_len, args.pred_len],
-        features=args.features,
-        target=args.target,
-        timeenc=timeenc,
-        freq=freq,
-        percent=percent,
-        seasonal_patterns=args.seasonal_patterns,
-        pretrain=pretrain
-    )
+    if data == 'Glucose':
+        data_set = Dataset_Combined(root_path='/home/yl2428/Time-LLM/dataset/glucose', 
+                            flag=flag, 
+                            target='LBSTRESN', 
+                            size=[args.seq_len, args.label_len, args.pred_len], normalization='global')
+    else:
+        data_set = Data(
+            root_path=args.root_path,
+            data_path=data_path,
+            flag=flag,
+            size=[args.seq_len, args.label_len, args.pred_len],
+            features=args.features,
+            target=args.target,
+            timeenc=timeenc,
+            freq=freq,
+            percent=percent,
+            seasonal_patterns=args.seasonal_patterns,
+            pretrain=pretrain
+        )
     data_loader = DataLoader(
         data_set,
         batch_size=batch_size,
